098990e0c6fcc1855ffd7318f9103fba1628dacf
Tanishq Dublish <tdublish_be21@thapar.edu>
2024-05-07 17:44:18 +0530

    fronturl

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use(cookieParser());
 
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: "https://e-commerce-zentacle-front.vercel.app/",
         credentials: true,
     })
 );
